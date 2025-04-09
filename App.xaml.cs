b8e8d2522d4706f52965b2c50b0a4657dda65ba0
Jan Cejka <honzacejkos@seznam.cz>
2025-04-09 19:01:30 +0200

    create repository

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CoordCalc
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
